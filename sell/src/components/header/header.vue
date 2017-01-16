b23c93a0135ba29768c45331adf3f5957df8b01c
wukai07 <wukai07@meituan.com>
2017-01-16 21:28:30 +0800

    ch6.2 -- add vue-resource

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    imported header
-  </div>
+<div>
+  imported header
+</div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+export default {}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
--- sell/src/main.js
+++ sell/src/main.js
@@ -1,28 +1,44 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import App from './App';
-import goods from './components/goods/goods';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import App from './App'
+import goods from 'components/goods/goods'
+import ratings from 'components/ratings/ratings'
+import seller from 'components/seller/seller'
 
-Vue.use(VueRouter);
+import 'common/stylus/index.styl'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
 
 /* eslint-disable no-unused-vars */
 const routes = [{
   path: '/goods',
   component: goods
-}];
+}, {
+  path: '/ratings',
+  component: ratings
+}, {
+  path: '/seller',
+  component: seller
+}]
 
-// 3. 创建 router 实例，然后传 `routes` 配置
+// 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
-  routes // （缩写）相当于 routes: routes
-});
+  routes, // （缩写）相当于 routes: routes
+  linkActiveClass: 'active'
+})
 
-// 4. 创建和挂载根实例。
+// 创建和挂载根实例。
 // 记得要通过 router 配置参数注入路由，
 // 从而让整个应用都有路由功能
 const app = new Vue({
   router,
   render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
+
+router.push('/goods')
+// export default app;
